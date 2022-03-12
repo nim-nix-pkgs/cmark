81c31dc362e6b00c1761cd8ece49e0ebc9e124a8
hugosenari <hugosenari@gmail.com>
2022-03-12 01:16:20 -0300

    chore: re index cmark

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''libcmark wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."cmark-master".url = "path:./master";
+  inputs."cmark-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cmark-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."cmark-0_1_0".url = "path:./0_1_0";
+  inputs."cmark-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cmark-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''libcmark wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-cmark-master.flake = false;
+  inputs.src-cmark-master.owner = "zengxs";
+  inputs.src-cmark-master.ref   = "refs/heads/master";
+  inputs.src-cmark-master.repo  = "nim-cmark";
+  inputs.src-cmark-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-cmark-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-cmark-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
